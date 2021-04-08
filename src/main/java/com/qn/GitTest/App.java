280f520de9ec2e3893d0e8b8cb0991e775ffbec7
shangqingnan <shangqn_wy@163.com>
2021-04-08 23:58:21 +0800

    第一次提交

---
+++ src/main/java/com/qn/GitTest/App.java
@@ -0,0 +1,13 @@
+package com.qn.GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
